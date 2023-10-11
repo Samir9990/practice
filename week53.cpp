a00f79ef7b3a1d314da4f21720d26e480b61d87e
Samir <145346890+Samir9990@users.noreply.github.com>
2023-10-11 11:01:59 +0500

    Update week53.cpp

--- week53.cpp
+++ week53.cpp
@@ -14,3 +14,20 @@ int main() {
 
     return 0;
 }
+
+#include <iostream>
+using namespace std;
+int main() {
+    
+    for (int i = 1; i <= 100; i++) {
+        if(i%5 == 0){
+            cout<<i<<endl;
+    }
+        else{
+            continue;
+        }
+        }
+    
+
+    return 0;
+}
